5efff20c3eb9a1b72c7c030ccdea6777d4f7c05f
natureoo <924943578@qq.com>
2023-04-04 17:49:08 +0800

    fix

--- docker-images/push_docker_images.sh
+++ docker-images/push_docker_images.sh
@@ -63,17 +63,17 @@ PushDockerImages () {
 
 	echo "Tagging Docker images..."
 	echo "$DOCKER_CMD image tag ${CORDA_IMAGE_PATH}:$CORDA_DOCKER_IMAGE_VERSION $DOCKER_REGISTRY/${CORDA_IMAGE_PATH}:$CORDA_DOCKER_IMAGE_VERSION"
-#	$DOCKER_CMD image tag ${CORDA_IMAGE_PATH}:$CORDA_DOCKER_IMAGE_VERSION $DOCKER_REGISTRY/${CORDA_IMAGE_PATH}:$CORDA_DOCKER_IMAGE_VERSION
+	$DOCKER_CMD image tag ${CORDA_IMAGE_PATH}:$CORDA_DOCKER_IMAGE_VERSION $DOCKER_REGISTRY/${CORDA_IMAGE_PATH}:$CORDA_DOCKER_IMAGE_VERSION
 #	$DOCKER_CMD tag ${CORDA_FIREWALL_IMAGE_PATH}:$FIREWALL_DOCKER_IMAGE_VERSION $DOCKER_REGISTRY/${CORDA_FIREWALL_IMAGE_PATH}_$VERSION:$FIREWALL_DOCKER_IMAGE_VERSION
-  docker image tag corda_image_ent:v1.00 europe-west2-docker.pkg.dev/canvas-hook-339503/corda/corda_image_ent:v1.00
+#  docker image tag corda_image_ent:v1.00 europe-west2-docker.pkg.dev/canvas-hook-339503/corda/corda_image_ent:v1.00
 
 	echo "Pushing Docker images to Docker repository..."
 	CORDA_DOCKER_REPOSITORY=$(echo  $DOCKER_REGISTRY/${CORDA_IMAGE_PATH}:$CORDA_DOCKER_IMAGE_VERSION 2>&1 | tr '[:upper:]' '[:lower:]')
 	echo "$CORDA_DOCKER_REPOSITORY"
 #	CORDA_FIREWALL_DOCKER_REPOSITORY=$(echo $DOCKER_REGISTRY/${CORDA_FIREWALL_IMAGE_PATH}_$VERSION:$FIREWALL_DOCKER_IMAGE_VERSION 2>&1 | tr '[:upper:]' '[:lower:]')
 	echo "Push for Corda Enterprise Docker image:"
-	docker push europe-west2-docker.pkg.dev/canvas-hook-339503/corda/corda_image_ent:v1.00
-#	$DOCKER_CMD push $CORDA_DOCKER_REPOSITORY
+#	docker push europe-west2-docker.pkg.dev/canvas-hook-339503/corda/corda_image_ent:v1.00
+	$DOCKER_CMD push $CORDA_DOCKER_REPOSITORY
 #	echo "Push for Corda Firewall Docker image:"
 #	$DOCKER_CMD push $CORDA_FIREWALL_DOCKER_REPOSITORY
 	echo "====== Pushing Docker images completed. ====== "
